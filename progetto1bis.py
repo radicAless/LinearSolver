146bbba314add88ccf59a368d81733054f9df2d4
Alessio Radice <ale.radice96@gmail.com>
2025-02-18 14:19:42 +0100

    Update progetto1bis.py

--- progetto1bis.py
+++ progetto1bis.py
@@ -5,29 +5,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io as sio
 
+# setting variables to solve system Ax=b 
 A = sio.mmread("dati/spa1.mtx").tocsr()
-# A = sio.mmread("dati/spa2.mtx").tocsr()
-# A = sio.mmread("dati/vem1.mtx").tocsr() #toeplitz/poisson/band?
-# A = sio.mmread("dati/vem2.mtx").tocsr() #toeplitz/poisson/band?
-
-# print(A.getnnz()/(A.get_shape()[1]**2))
-# print(A.get_shape())
-# print(np.linalg.cond(A.todense()))
-# plt.spy(A)
-# plt.show()
-
-# def is_diagonal(A):
-#     A = np.abs(np.asarray(A))  # converts lists to numpy arrays
-#     diagonal = np.diag(A)  # [7, 7, 7]
-#     others = A - np.diag(diagonal)  # matrix 'A' but with zeros along the diagonal
-#     return np.all(diagonal >= others.sum(axis=1))
-# print(is_diagonal(A.todense()))
-
 x = np.ones(A.get_shape()[1])
 b = A*x
+# setting thresholds for the methods to stop
 tol = [1.e-04, 1.e-06, 1.e-08, 1.e-10]
-metodo = [linSolving.Jacobi(A,b), linSolving.Gradiente(A,b), linSolving.Coniugato(A,b), linSolving.Seidel(A,b)]
 maxIter = 3.e+4
+# start comparison between these four methods
+metodo = [linSolving.Jacobi(A,b), linSolving.Gradiente(A,b), linSolving.Coniugato(A,b), linSolving.Seidel(A,b)]
 
 for j in range(0,len(metodo)):
  print("Metodo: " + str(metodo[j].__class__.__name__))
@@ -41,7 +27,6 @@ for j in range(0,len(metodo)):
     print("Errore relativo = " + str(er))
     print("Numero iterazioni = " + str(results[1]))
     print("Tempo di calcolo = " + str(comp_time))
-    # print(er,results[1],comp_time)
 
 
 
